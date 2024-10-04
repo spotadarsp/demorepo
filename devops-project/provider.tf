509e448a53f7cb16f2a0833519022030b6750b01
shubham <spotadar.sp@gmail.com>
2024-10-05 00:07:15 +0530

    modified p

--- devops-project/provider.tf
+++ devops-project/provider.tf
@@ -1,10 +1,9 @@
-provider "aws" {
-    region = "ap-south-1"
-    profile = "shubham"
-}
-
 # provider "aws" {
-#   region     = "us-east-1"  # Specify your desired AWS region
-#   access_key = var.aws_access_key_id
-#   secret_key = var.aws_secret_access_key
+#     region = "ap-south-1"
+#     profile = "shubham"
 # }
+
+provider "aws" {
+  region     = "ap-south-1"  # Specify your desired AWS region
+ shared_credentials_files = ["C:/Users/spota/.aws/credentials"]
+}
